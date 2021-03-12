4752429655b9f28e41e45b100c6e9090346310b3
juanlulch <juanlulch@gmail.com>
2021-03-12 18:26:54 +0100

    "Líneas en método main"

--- JuanLuisLeoChanclonDAM/src/JuanLuis.java
+++ JuanLuisLeoChanclonDAM/src/JuanLuis.java
@@ -3,6 +3,10 @@ public class JuanLuis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Validando proyecto");
+		System.out.println("linea 1 mas");
+		System.out.println("linea 2 mas");
+
 
 	}
 
